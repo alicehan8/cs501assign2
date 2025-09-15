be41dd0a15be1a3c1ad5e14506aa93dd69598afa
alicehan8 <alicehan@bu.edu>
2025-09-15 16:29:00 -0400

    added modifiers

--- app/src/main/java/com/example/assign2/MainActivity.kt
+++ app/src/main/java/com/example/assign2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,9 +17,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.assign2.ui.theme.Assign2Theme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,9 +55,9 @@ fun ColorCard(color: Color, label: String, modifier: Modifier) {
         colors = CardDefaults.cardColors(
             containerColor = color
         ),
-        modifier = Modifier.size(width=240.dp, height=100.dp)
+        modifier = modifier.size(width=240.dp, height=100.dp)
     ){
-        Text(label)
+        Text(label, textAlign = TextAlign.Center, modifier = Modifier.padding(16.dp))
     }
 }
 
@@ -62,9 +66,11 @@ fun ColorCard(color: Color, label: String, modifier: Modifier) {
 fun GreetingPreview() {
     Assign2Theme {
         Column() {
-            ColorCard(Color.Red, "red", Modifier)
-            ColorCard(Color.Blue, "blue", Modifier)
-            ColorCard(Color.Green, "green", Modifier)
+            ColorCard(Color.Red, "red", Modifier
+                .padding(5.dp)
+                .border(4.dp, Color.Black))
+            ColorCard(Color.Blue, "blue", Modifier.background(Color.Cyan).size(width=100.dp, height=100.dp))
+            ColorCard(Color.Green, "green", Modifier.background(Color.Yellow).padding(5.dp))
         }
     }
 }
